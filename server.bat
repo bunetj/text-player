1e12acd027fb6f660427e3a37431248a94ba6b0a
bunetj <245584680+bunetj@users.noreply.github.com>
2026-09-21 05:25:50 +0400

    x

--- server.bat
+++ server.bat
@@ -2,7 +2,8 @@
 cd /d "%~dp0"
 echo Serving from: %CD%
 echo.
-echo   text player: http://localhost:8731/
+echo   Cheatread:  http://localhost:8731/CHEATREAD/telegram/index.html
+echo   Roleplay:   http://localhost:8731/SELF-PLAY/TELEGRAM/index.html
 echo.
 echo Ctrl+C to stop.
 echo.
